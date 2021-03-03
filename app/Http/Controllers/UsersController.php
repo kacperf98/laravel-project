333aff0ac8bef04e66b278284e543edf3ef04273
Kacper <kfirlej98@gmail.com>
2021-03-03 18:21:22 +0100

    Initial commit

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+
+class UsersController extends Controller
+{
+    public function index()
+    {
+        $users = User::all();
+
+        return view('/index', compact('users'));
+    }
+
+    public function store()
+    {
+        $user = new User;
+
+        $user->name = request('name');
+        $user->email = request('email');
+        $user->password = bcrypt(request('password'));
+        $user->save();
+
+        return back();
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>All Users</h1>
+
+    @foreach ($users as $user)
+        <li>{{ $user->name }}</li>
+    @endforeach
+
+    <h2>Add User</h2>
+
+    <form method="POST" action="/index">
+        {{ csrf_field() }}
+
+        <p>
+            <input type="text" name="name" placeholder="Name" required>
+        </p>
+
+        <p>
+            <input type="email" name="email" placeholder="Email Address" required>
+        </p>
+
+        <p>
+            <input type="password" name="password" placeholder="Password" required>
+        </p>
+
+        <p>
+            <button type="submit">Add User</button>
+        </p>
+    </form>
+
+</body>
+</html>
