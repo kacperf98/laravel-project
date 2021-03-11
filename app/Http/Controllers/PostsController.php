d94bbb526cad14046713195139afa23e4d985987
Kacper <kfirlej98@gmail.com>
2021-03-11 23:14:15 +0100

    Update PostsController.php

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use DB;
 use App\Models\Post;
 
 class PostsController extends Controller
 {
+    /**
+     *
+     * Show a list of all Posts.
+     *
+     */
     public function index()
     {
         $posts = Post::latest()->get();
@@ -14,10 +18,65 @@ class PostsController extends Controller
         return view('post', ['posts' => $posts]);
     }
 
+    /**
+     *
+     * Show a single Post.
+     *
+     */
     public function show($id)
     {
         $post = Post::find($id);
 
         return view('posts/show', ['post' => $post]);
+    }
+
+    /**
+     *
+     * Show a view to create a new Post.
+     *
+     */
+    public function create()
+    {
+
+    }
+
+    /**
+     *
+     * Store the new created Post.
+     *
+     */
+    public function store()
+    {
+
+    }
+
+    /**
+     *
+     * Show a view to edit Post.
+     *
+     */
+    public function edit()
+    {
+
+    }
+
+    /**
+     *
+     * Store the edited Post.
+     *
+     */
+    public function update()
+    {
+
+    }
+
+    /**
+     *
+     * Delete the Post.
+     *
+     */
+    public function destroy()
+    {
+
     }
 }
