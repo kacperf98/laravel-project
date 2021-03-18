844e130088d8a1c0b2caa85134cf5c0cafff2a5b
Kacper <kfirlej98@gmail.com>
2021-03-18 20:13:51 +0100

    Factory

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class PostFactory extends Factory
@@ -22,7 +23,10 @@ class PostFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'title' => $this->faker->sentence,
+            'excerpt' => $this->faker->sentence,
+            'body' => $this->faker->paragraph
         ];
     }
 }
