f815e1e7564c0134ed970ad1140ea12683297961
Kacper <kfirlej98@gmail.com>
2021-03-24 21:21:33 +0100

    Update layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -57,8 +57,9 @@ Released   : 20131203
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
+                                                     document.getElementById('logout-form').submit();"
+                                    style="margin: 0; color: #333333;">
+                                        {{ __('Logout') }}
                                 </a>
 
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
