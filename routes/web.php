ed56f0aee73831d78c907fe920cbbad402e44771
Kacper <kfirlej98@gmail.com>
2021-03-24 20:52:59 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -20,16 +20,15 @@ Route::view('about', 'about');
 
 Route::get('posts', [PostsController::class, 'index'])->name('posts.index');
 Route::post('posts', [PostsController::class, 'store']);
-Route::get('posts/create', [PostsController::class, 'create']);
+Route::get('posts/create', [PostsController::class, 'create'])->middleware('auth');
 Route::get('posts/{post}', [PostsController::class, 'show'])->name('posts.show');
-Route::get('posts/{post}/edit', [PostsController::class, 'edit']);
+Route::get('posts/{post}/edit', [PostsController::class, 'edit'])->middleware('auth');
 Route::put('posts/{post}', [PostsController::class, 'update']);
 
 
 Route::get('users', [UsersController::class, 'index']);
 Route::post('users', [UsersController::class, 'store']);
-Route::get('users/create', [UsersController::class, 'create']);
-
+Route::get('users/create', [UsersController::class, 'create'])->middleware('auth');
 
 Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index']);
